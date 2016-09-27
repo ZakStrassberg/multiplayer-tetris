959c7c485d7e99cb21b4df66af298a5f2b67b870
Zak Strassberg <zakstrassberg@gmail.com>
2016-09-27 14:56:25 -0700

    completed addLine() feature

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express  = require( 'express' ),
-    path     = require( 'path' ),
-    root     = __dirname,
-    port     = process.env.PORT || 8000,
-    bp       = require('body-parser'),
-    app      = express(),
-    players  = [];
+path     = require( 'path' ),
+root     = __dirname,
+port     = process.env.PORT || 8000,
+bp       = require('body-parser'),
+app      = express(),
+players  = [];
 
 
 app.use( express.static( path.join( root, 'client' )));
@@ -15,44 +15,49 @@ app.use(bp.json())
 // require('./server/config/routes.js')(app);
 
 var server = app.listen( port, function() {
-    console.log( 'server running on port ${ port }' );
+  console.log( 'server running on port ${ port }' );
 });
 
 var io       = require('socket.io').listen(server)
 
 io.sockets.on('connection', function (socket) {
-    console.log("WE ARE USING SOCKETS!");
-    console.log(socket.id);
+  console.log("WE ARE USING SOCKETS!");
+  console.log(socket.id);
 
-    socket.on('start game', function() {
-        console.log(socket.id, 'joined the game')
-        players.push({id: socket.id, score: 0})
-    })
+  socket.on('start game', function(name) {
+    console.log(socket.id, 'joined the game')
+    players.push({id: socket.id, name: name, score: 0, rows: 0})
+  })
 
-    socket.on('end game', function(score) {
-        console.log(socket.id, 'lost the game with', score, 'points')
-        players.splice(players.indexOf(socket.id), 1)
-    })
+  socket.on('end game', function(score) {
+    console.log(socket.id, 'lost the game with', score, 'points')
+    players.splice(players.indexOf(socket.id), 1)
+  })
 
-    socket.on('score', function(score) {
-        for (var player of players) {
-            if (player.id == socket.id) {
-                player.score = score;
-            }
-        }
-    })
+  socket.on('score', function(score) {
+    for (var player of players) {
+      if (player.id == socket.id) {
+        player.score = score;
+      }
+    }
+  })
 
-    socket.on('disconnect', function() {
-        players.splice(players.indexOf(socket.id), 1)
-        console.log(socket.id, 'disconnected')
-    })
+  socket.on('row', function(rows) {
+    console.log("ROW!!!!!")
+    for (var player of players) {
+      if (player.id == socket.id) {
+        player.rows = rows;
+      }
+    }
+  })
 
-    setInterval(function() {
-        io.emit('update scoreboard', players)
-    }, 1000)
-
+  socket.on('disconnect', function() {
+    players.splice(players.indexOf(socket.id), 1)
+    console.log(socket.id, 'disconnected')
+  })
 })
 
-// setInterval(function() {
-//     console.log(players)
-// }, 10000)
+setInterval(function() {
+  io.emit('updateScoreboard', players)
+  // console.log(players)
+}, 1000)
