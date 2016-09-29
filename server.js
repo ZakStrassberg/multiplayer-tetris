40ac12be855c35f528ea79bf3c703b55e2a6cab6
Zak Strassberg <zakstrassberg@gmail.com>
2016-09-29 12:10:09 -0700

    clean up server.js and add more activity events

--- server.js
+++ server.js
@@ -19,23 +19,33 @@ app.use(bp.json())
 // require('./server/config/routes.js')(app);
 
 var server = app.listen( port, function() {
-  console.log( 'server running on port ${ port }' );
+  console.log( 'server running on port', port );
 });
 
-var io       = require('socket.io').listen(server)
+var io = require('socket.io').listen(server)
 
 io.sockets.on('connection', function (socket) {
   var socketId = socket.id;
   console.log(socketId, "connected")
 
   socket.on('start game', function(name) {
-    console.log(socket.id, 'joined the game')
-    players.push({id: socket.id, name: name, score: 0, rows: 0})
+    console.log(name, socketId, 'joined the game')
+    let index = players.push({id: socketId, name: name, score: 0, rows: 0})
+    io.emit('updateActivity', players[index].name + " has started a game.")
   })
 
   socket.on('end game', function(score) {
-    console.log(socket.id, 'lost the game with', score, 'points')
-    players.splice(players.indexOf(socket.id), 1)
+    for (var player of players) {
+      if (player.id == socketId) {
+        var endingPlayer = player
+      }
+    }
+    if (endingPlayer){
+      io.emit('updateActivity', endingPlayer.name + " has lost with " + endingPlayer.score + " points.")
+      players.splice(players.indexOf(endingPlayer), 1)
+    } else {
+      console.log("something went wrong. a player left but I couldn't find them.")
+    }
   })
 
   socket.on('score', function(score) {
@@ -56,27 +66,30 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('sendLines', function(n) {
     if (players.length > 1) {
-      console.log(n, 'lines sent')
+      console.log(n, 'lines being sent:')
       var sender;
       for (var player of players) {
         if (player.id == socket.id) {
           sender = player;
         }
       }
-      for (var i = 1; i < n; i++) {
-        randomPlayer = players[Math.floor(Math.random()*players.length)]
-        var maxTries = 0;
-        while (randomPlayer.id == socketId && maxTries < 5) {
+      if (sender) {
+        for (var i = 1; i < n; i++) {
           randomPlayer = players[Math.floor(Math.random()*players.length)]
-          maxTries++
-        }
-        socket.broadcast.to(randomPlayer.id).emit('addLine')
-        try {
-          console.log(sender.name, "is sending line to", randomPlayer.name)
-        }
-        catch(err) {
+          var maxTries = 5;
+          // prevent from sending line to self:
+          while (randomPlayer.id == socketId && maxTries > 0) {
+            randomPlayer = players[Math.floor(Math.random()*players.length)]
+            maxTries--
+          }
+          socket.broadcast.to(randomPlayer.id).emit('addLine')
+          try {
+            io.emit('updateActivity', sender.name + " has sent a line to " + randomPlayer.name)
+          }
+          catch(err) {
             console.log("ERROR:")
             console.log(err)
+          }
         }
       }
     }
@@ -84,14 +97,14 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('disconnect', function() {
     for (var player of players) {
-        if (player.id == socket.id) {
-          var disconnectingPlayer = player
-        }
+      if (player.id == socketId) {
+        var disconnectingPlayer = player
       }
+    }
     if (disconnectingPlayer){
-      io.emit('updateActivity', disconnectingPlayer.name + " has left the game.")
+      io.emit('updateActivity', disconnectingPlayer.name + " has disconnected.")
       console.log(socket.id, 'disconnected')
-      players.splice(players.indexOf(socketId), 1)
+      players.splice(players.indexOf(disconnectingPlayer), 1)
     }
   })
 
